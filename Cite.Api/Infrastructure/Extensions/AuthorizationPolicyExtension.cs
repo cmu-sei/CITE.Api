fa234777fd6a2531367cfa08bd74d08a94b66236
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2022-07-25 16:23:40 -0400

    standard scopes, clients and identity

--- Cite.Api/Infrastructure/Extensions/AuthorizationPolicyExtension.cs
+++ Cite.Api/Infrastructure/Extensions/AuthorizationPolicyExtension.cs
@@ -1,6 +1,7 @@
 // Copyright 2022 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license, please see LICENSE.md in the project root for license information or contact permission@sei.cmu.edu for full terms.
 
+using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Cite.Api.Infrastructure.Authorization;
@@ -9,9 +10,16 @@ namespace Cite.Api.Infrastructure.Extensions
 {
     public static class AuthorizationPolicyExtensions
     {
-        public static void AddAuthorizationPolicy(this IServiceCollection services)
+        public static void AddAuthorizationPolicy(this IServiceCollection services, Options.AuthorizationOptions authOptions)
         {
-            services.AddAuthorization();
+            services.AddAuthorization(options =>
+            {
+                // Require all scopes in authOptions
+                var policyBuilder = new AuthorizationPolicyBuilder().RequireAuthenticatedUser();
+                Array.ForEach(authOptions.AuthorizationScope.Split(' '), x => policyBuilder.RequireClaim("scope", x));
+
+                options.DefaultPolicy = policyBuilder.Build();
+            });
             services.AddSingleton<IAuthorizationHandler, FullRightsHandler>();
             services.AddSingleton<IAuthorizationHandler, ContentDeveloperHandler>();
             services.AddSingleton<IAuthorizationHandler, OperatorHandler>();
