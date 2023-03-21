70a4298fbe40226eca364ac8137ea2733ce7acc2
sei-tspencer <trspencer@sei.cmu.edu>
2023-03-21 16:51:55 -0400

    add team cascade delete

---
+++ Cite.Api.Migrations.PostgreSQL/Migrations/20230321154823_team-cascade-delete.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cite.Api.Migrations.PostgreSQL.Migrations
+{
+    public partial class teamcascadedelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_teams_evaluations_evaluation_id",
+                table: "teams");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_teams_evaluations_evaluation_id",
+                table: "teams",
+                column: "evaluation_id",
+                principalTable: "evaluations",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_teams_evaluations_evaluation_id",
+                table: "teams");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_teams_evaluations_evaluation_id",
+                table: "teams",
+                column: "evaluation_id",
+                principalTable: "evaluations",
+                principalColumn: "id");
+        }
+    }
+}
