5d9bb32bdf048f29acb389f1f25e787be952ad9b
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2023-05-09 15:04:38 -0400

    Cwdoe 810 app versions (#16)
    Co-authored-by: sei-tspencer <trspencer@sei.cmu.edu>

--- Cite.Api/Controllers/HealthCheckController.cs
+++ Cite.Api/Controllers/HealthCheckController.cs
@@ -3,52 +3,84 @@
 
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Cite.Api.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Swashbuckle.AspNetCore.Annotations;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Cite.Api.Controllers
 {
-    [Route("api/")]
-    [ApiController]
-    public class HealthCheckController : ControllerBase
+    [AllowAnonymous]
+    public class HealthCheckController : BaseController
     {
-        private readonly CiteContext _context;
+        private readonly HealthCheckService healthCheckService;
 
-        public HealthCheckController(
-            CiteContext context
-        )
+        public HealthCheckController(HealthCheckService healthCheckService)
         {
-            _context = context;
+            this.healthCheckService = healthCheckService;
         }
 
         /// <summary>
-        /// Responds when this api is functional
+        /// Checks the liveliness health endpoint
         /// </summary>
         /// <remarks>
-        /// Returns a health message.
-        /// <para />
-        /// No user authentication is required
+        /// Returns a HealthReport of the liveliness health check
         /// </remarks>
         /// <returns></returns>
-        [HttpGet("healthcheck")]
-        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
-        [SwaggerOperation(OperationId = "healthCheck")]
-        public async Task<IActionResult> HealthCheck(CancellationToken ct)
+        [HttpGet("health/live")]
+        [ProducesResponseType(typeof(HealthReport), (int)HttpStatusCode.OK)]
+        [SwaggerOperation(OperationId = "getLiveliness")]
+        public async Task<IActionResult> GetLiveliness(CancellationToken ct)
         {
-            var healthMessage = "It is well";
-            try
+            HealthReport report = await this.healthCheckService.CheckHealthAsync((check) => check.Tags.Contains("live"));
+            var result = new
             {
-                var dbCheck = await _context.Users.Select(g => g.Id).FirstAsync();
-            }
-            catch (System.Exception ex)
+                status = report.Status.ToString()
+            };
+            return report.Status == HealthStatus.Healthy ? this.Ok(result) : this.StatusCode((int)HttpStatusCode.ServiceUnavailable, result);
+        }
+
+        /// <summary>
+        /// Checks the readiness health endpoint
+        /// </summary>
+        /// <remarks>
+        /// Returns a HealthReport of the readiness health check
+        /// </remarks>
+        /// <returns></returns>
+        [HttpGet("health/ready")]
+        [ProducesResponseType(typeof(HealthReport), (int)HttpStatusCode.OK)]
+        [SwaggerOperation(OperationId = "getReadiness")]
+        public async Task<IActionResult> GetReadiness(CancellationToken ct)
+        {
+            HealthReport report = await this.healthCheckService.CheckHealthAsync((check) => check.Tags.Contains("ready"));
+            var result = new
             {
-                healthMessage = "I'm sorry, but I currently can't access the database.  " + ex.Message;
-            }
-            return Ok(healthMessage);
+                status = report.Status.ToString()
+            };
+            return report.Status == HealthStatus.Healthy ? this.Ok(result) : this.StatusCode((int)HttpStatusCode.ServiceUnavailable, result);
+        }
+
+        /// <summary>
+        /// Returns the current version of the API
+        /// </summary>
+        /// <remarks>
+        /// Returns the version.
+        /// </remarks>
+        /// <returns></returns>
+        [HttpGet("version")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        [SwaggerOperation(OperationId = "getVersion")]
+        public async Task<IActionResult> GetVersion(CancellationToken ct)
+        {
+            var version = (AssemblyInformationalVersionAttribute)Assembly
+                .GetExecutingAssembly()
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)
+                .FirstOrDefault();
+
+            return Ok(version.InformationalVersion);
         }
 
     }
