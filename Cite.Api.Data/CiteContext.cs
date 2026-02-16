e093fde1115e90c1bbe39ebfdd6cbf6540482546
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2026-02-16 14:32:25 +0000

    remove duplicate using

--- Cite.Api.Data/CiteContext.cs
+++ Cite.Api.Data/CiteContext.cs
@@ -2,7 +2,6 @@
 // Released under a MIT (SEI)-style license, please see LICENSE.md in the project root for license information or contact permission@sei.cmu.edu for full terms.
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
